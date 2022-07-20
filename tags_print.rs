b1eef3888afd3309f110415552e59de811132bfd
João Lucas Cruz <63518059+jaotuta@users.noreply.github.com>
2022-07-20 15:09:15 -0300

    Update tags_print.rs
    add new loop

--- tags_print.rs
+++ tags_print.rs
@@ -1,13 +1,25 @@
 fn main() {
-    let lumi_inicial = 1;
-    let lumi_final = 12;
-    let circuito = 1;
+
+    let mut lumi_inicial = 1;
+    let mut lumi_final = 6;
+    let mut circuito = 1;
     let mut i = lumi_inicial;
+    
     while i <= lumi_final {
             println!("C{}.L{}:(+)" ,circuito ,i );
             println!("C{}.L{}:(-)" ,circuito ,i );
             i = i + 1;
     }
 
+    lumi_inicial = 7;
+    lumi_final = 12;
+    circuito = 2;
+    i = lumi_inicial;
+    
+    while i <= lumi_final {
+            println!("C{}.L{}:(+)" ,circuito ,i );
+            println!("C{}.L{}:(-)" ,circuito ,i );
+            i = i + 1;
+    }
 
 }
